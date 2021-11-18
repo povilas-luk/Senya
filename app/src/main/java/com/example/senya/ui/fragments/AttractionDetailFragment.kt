206b834a285c6bcdebdb535a2d24d2c1b303a290
50kw <povilas.lukasevicius@gmail.com>
2021-11-18 16:07:29 +0200

    nav graph - action, arguments

---
+++ app/src/main/java/com/example/senya/ui/fragments/AttractionDetailFragment.kt
@@ -0,0 +1,24 @@
+package com.example.senya.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.senya.R
+
+class AttractionDetailFragment : Fragment(R.layout.fragment_attraction_detail) {
+
+    /*override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_attraction_detail, container, false)
+    }*/
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
