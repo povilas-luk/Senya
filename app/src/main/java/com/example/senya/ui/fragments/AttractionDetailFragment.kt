a40c2472e320e889338ef08f71529d8cdb45661d
50kw <povilas.lukasevicius@gmail.com>
2021-11-19 22:48:24 +0200

    menu item to open google maps with coordinates

--- app/src/main/java/com/example/senya/ui/fragments/AttractionDetailFragment.kt
+++ app/src/main/java/com/example/senya/ui/fragments/AttractionDetailFragment.kt
@@ -1,11 +1,12 @@
 package com.example.senya.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.navArgs
+import com.example.senya.R
 import com.example.senya.data.Attraction
 import com.example.senya.databinding.FragmentAttractionDetailBinding
 import com.squareup.picasso.Picasso
@@ -20,6 +21,11 @@ class AttractionDetailFragment : BaseFragment() {
     private val attraction: Attraction by lazy {attractionsList.find {
             it.id == safeArgs.attractionId
         }?: Attraction()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -43,6 +49,26 @@ class AttractionDetailFragment : BaseFragment() {
             factsDialog()
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_attraction_detail, menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.locationMenuItem -> {
+                val uri = "geo:${attraction.location.latitude},${attraction.location.longitude}?z=9&q=${attraction.title}"
+                val gmmIntentUri = Uri.parse(uri)
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                startActivity(mapIntent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun factsDialog() {
